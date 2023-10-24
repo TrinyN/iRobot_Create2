9cc727698eeab7f5f44bfc2bfb7d692b1b52d9f7
escott556 <123144924+escott556@users.noreply.github.com>
2023-10-23 20:18:09 -0400

    Update Project4.py
    basic keys, adjusted roomba image to center. no luck with rotation

--- project4/Project4.py
+++ project4/Project4.py
@@ -11,6 +11,7 @@ Last Editted:
 import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
+from pynput import keyboard
 
 root = Tk()  # create a root widget
 root.title("Roomba")
@@ -19,6 +20,7 @@ root.minsize(1920, 1080)  # width, height
 root.maxsize(1920, 1080)
 root.geometry("1920x1080+0+0")  # width x height + x + y
 root.resizable=FALSE
+root.state('zoomed') # automatically fullscreen
 
 frame = Frame(root)
 frame.pack()
@@ -27,13 +29,34 @@ frame.pack()
 canvas = Canvas(frame, bg="purple", width=1920, height=1080)
 canvas.pack()
 
-imgpath = 'C:/Users/escot/Downloads/create2-removebg-preview.png'
+imgpath = 'C:/Users/escot/Downloads/create2sm.png'
 roombaPic = PhotoImage(file=imgpath)
-canvas.create_image(500,500,image=roombaPic)
+canvas.create_image(770,440,image=roombaPic)
 
-# imgpath = 'C:/Users/escot/Downloads/create2-removebg-preview.png'
-# image = PhotoImage(file=imgpath)
-# image = image.subsample(25)
-# img = Label(root, image=image)
-# img.pack()
+################################## keys ###################
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        return False #stops listener
+    try:
+        k = key.char #single-char-keys
+    except:
+        k = key.name #other keys
+        print(k + " not allowed")
+    if k in ['w','a','s','d']:          #keys of interest
+        if k == 'w':
+            print('moving forward')
+        if k == 'a':
+            print('moving left')
+        if k == 's':
+            print('moving right')
+        if k == 'd':
+            print('moving backward')
+        print('Key pressed: ' + k)
+
+    
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+listener.join()
+
 root.mainloop()
