7320f61fbfdf2e96d852873118054b76245ef680
escott556 <123144924+escott556@users.noreply.github.com>
2023-10-24 13:49:03 -0400

    Update Project4.py

--- project4/Project4.py
+++ project4/Project4.py
@@ -12,10 +12,12 @@ import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
 from pynput import keyboard
+from functools import partial
 import keyboard
 import threading
+from Robot import Robot
 
-robot = Robot("COM5")
+robot = Robot("COM11")
 robot.start()
 robot.safe()
 
@@ -35,9 +37,26 @@ frame.pack()
 canvas = Canvas(frame, bg="purple", width=1920, height=1080)
 canvas.pack()
 
-imgpath = 'C:/Users/escot/Downloads/create2sm.png'
-roombaPic = PhotoImage(file=imgpath)
-canvas.create_image(770,440,image=roombaPic)
+# Creating images at different angles
+pil_roombaPic_N = Image.open('create2sm.png')
+pil_roombaPic_NE = pil_roombaPic_N.rotate(45)
+pil_roombaPic_E = pil_roombaPic_N.rotate(90)
+pil_roombaPic_SE = pil_roombaPic_N.rotate(135)
+pil_roombaPic_S = pil_roombaPic_N.rotate(180)
+pil_roombaPic_SW = pil_roombaPic_N.rotate(225)
+pil_roombaPic_W = pil_roombaPic_N.rotate(270)
+pil_roombaPic_NW = pil_roombaPic_N.rotate(315)
+
+roombaPic_N = ImageTk.PhotoImage(pil_roombaPic_N)
+roombaPic_NE = ImageTk.PhotoImage(pil_roombaPic_NW)
+roombaPic_E = ImageTk.PhotoImage(pil_roombaPic_W)
+roombaPic_SE = ImageTk.PhotoImage(pil_roombaPic_SW)
+roombaPic_S = ImageTk.PhotoImage(pil_roombaPic_S)
+roombaPic_SW = ImageTk.PhotoImage(pil_roombaPic_SE)
+roombaPic_W = ImageTk.PhotoImage(pil_roombaPic_E)
+roombaPic_NW = ImageTk.PhotoImage(pil_roombaPic_NE)
+
+canvas.create_image(770,440,image=roombaPic_N)
 
 #######LED Buttons######
 def LED(color):
@@ -96,34 +115,34 @@ D = FALSE
 # w_button.bind('<ButtonRelease-1>', lambda event: handle_w_button_release())
     
 
-# Create buttons with text labels
-w_button = Button(wasd_frame, text="W", command=lambda: handle_input('W'), width=6, height=3)
-a_button = Button(wasd_frame, text="A", command=lambda: handle_input('A'), width=6, height=3)
-s_button = Button(wasd_frame, text="S", command=lambda: handle_input('S'), width=6, height=3)
-d_button = Button(wasd_frame, text="D", command=lambda: handle_input('D'), width=6, height=3)
+# # Create buttons with text labels
+# w_button = Button(wasd_frame, text="W", command=lambda: handle_input('W'), width=6, height=3)
+# a_button = Button(wasd_frame, text="A", command=lambda: handle_input('A'), width=6, height=3)
+# s_button = Button(wasd_frame, text="S", command=lambda: handle_input('S'), width=6, height=3)
+# d_button = Button(wasd_frame, text="D", command=lambda: handle_input('D'), width=6, height=3)
 
-# Position buttons to resemble a keyboard layout
-w_button.grid(row=0, column=1, padx=10)
-a_button.grid(row=1, column=0, pady=10)
-s_button.grid(row=1, column=1, pady=10)
-d_button.grid(row=1, column=2, pady=10)
+# # Position buttons to resemble a keyboard layout
+# w_button.grid(row=0, column=1, padx=10)
+# a_button.grid(row=1, column=0, pady=10)
+# s_button.grid(row=1, column=1, pady=10)
+# d_button.grid(row=1, column=2, pady=10)
 
 
-def handle_input(key):
-    if key == 'W':
-        print("Driving Forward...")
+# def handle_input(key):
+#     if key == 'W':
+#         print("Driving Forward...")
 
-    elif key == 'A':
-        print("Driving Left...")
+#     elif key == 'A':
+#         print("Driving Left...")
 
-    elif key == 'S':
-        print("Driving Backwards...")
+#     elif key == 'S':
+#         print("Driving Backwards...")
 
-    elif key == 'D':
-        print("Driving Right...")
+#     elif key == 'D':
+#         print("Driving Right...")
 
-    else:
-        print("stop!")
+#     else:
+#         print("stop!")
 
 def handle_keyboard_input():
     global W, A, S, D
@@ -141,15 +160,13 @@ def handle_keyboard_input():
             if not W:
                 W = True
                 # handle_input('W')
-                robot.start()
-                robot.safe()
                 # robot.driveDirect(b'x00', b'xC8', b'x00', b'xC8')
                 robot.driveDirect(b'\x00', b'\x64', b'\x00', b'\x64')
 
                 # connection.write(b'\x80\x83')
                 # time.sleep(0.5)
                 # connection.write(DRIVE_FORWARD_200)
-
+                canvas.create_image(770,440,image=roombaPic_N)
                 print("Driving Forward...")
 
         elif (
@@ -161,13 +178,11 @@ def handle_keyboard_input():
             if not S:
                 S = True
                 # handle_input('S')
+                canvas.create_image(770,440,image=roombaPic_S)
                 print("Driving Backwards...")
-                robot.start()
-                robot.safe()
                 # robot.drive(b'xFF', b'x38', b'x7F', b'xFF')
                 # robot.driveDirect(b'xFF', b'x38', b'xFF', b'x38')
                 robot.driveDirect(b'\xFF', b'\xC0', b'\xFF', b'\xC0')
-
                 # connection.write(b'\x80\x83')
                 # time.sleep(0.5)
                 # connection.write(DRIVE_BACKWARDS_200)
@@ -183,6 +198,8 @@ def handle_keyboard_input():
             if not A:
                 A = True
                 # handle_input('A')
+                canvas.create_image(770,440,image=roombaPic_W)
+
                 print("Driving Left...")
 
         elif (
@@ -194,30 +211,35 @@ def handle_keyboard_input():
             if not D:
                 D = True
                 # handle_input('D')
+                canvas.create_image(770,440,image=roombaPic_E)
                 print("Driving Right...")
 
         elif keyboard.is_pressed("w") and keyboard.is_pressed("a"):
             if not W or not A:
                 W = True
                 A = True
+                canvas.create_image(770,440,image=roombaPic_NW)
                 print("W and A Driving...")
 
         elif keyboard.is_pressed("w") and keyboard.is_pressed("d"):
             if not W or not D:
                 W = True
                 D = True
+                canvas.create_image(770,440,image=roombaPic_NE)
                 print("W and D Driving...")
 
         elif keyboard.is_pressed("s") and keyboard.is_pressed("a"):
             if not S or not A:
                 S = True
                 A = True
+                canvas.create_image(770,440,image=roombaPic_SW)
                 print("S and A Driving...")
 
         elif keyboard.is_pressed("s") and keyboard.is_pressed("d"):
             if not S or not D:
                 S = True
                 D = True
+                canvas.create_image(770,440,image=roombaPic_SE)
                 print("S and D Driving...")
 
         elif keyboard.is_pressed("esc"):
@@ -231,41 +253,14 @@ def handle_keyboard_input():
                 D = False
 
                 # handle_input("null")
-                # robot.driveDirect(b'x00', b'x00', b'x00', b'x00')
+                robot.driveDirect(b'\x00', b'\x00', b'\x00', b'\x00')
                 # DRIVE_ONE_WHEEL_200 = b'\x86'
-                robot.stop()
+                # robot.stop()
                 # connection.write(b'\xAD')
                 print("Stop!")
 
 
 keyboard_thread = threading.Thread(target=handle_keyboard_input)
 keyboard_thread.start()
-
-
-################################## keys ###################
-
-def on_press(key):
-    if key == keyboard.Key.esc:
-        return False #stops listener
-    try:
-        k = key.char #single-char-keys
-    except:
-        k = key.name #other keys
-        print(k + " not allowed")
-    if k in ['w','a','s','d']:          #keys of interest
-        if k == 'w':
-            print('moving forward')
-        if k == 'a':
-            print('moving left')
-        if k == 's':
-            print('moving right')
-        if k == 'd':
-            print('moving backward')
-        print('Key pressed: ' + k)
-
-    
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-listener.join()
 
 root.mainloop()
